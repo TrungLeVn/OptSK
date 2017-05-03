66cf0b39993834548aaa02af36a6ca7f32436918
Trung Le <trunglehai.hvnh@gmail.com>
2017-05-03 11:27:56 +0100

    Add trapezoidal method, which is much faster

--- R/ImpliedMoments.R
+++ R/ImpliedMoments.R
@@ -1,54 +1,113 @@
 #' @export ImpliedMoments
-ImpliedMoments <- function(optdta,moments = NULL,cluster = NULL){
+#' @import parallel
+#' @import foreach
+#' @import doParallel
+ImpliedMoments <- function(optdta,moments = NULL,method = NULL,cluster = NULL){
   if(is.null(moments)) ExpChoice = c("Var","Skewness","Kurtosis") else ExpChoice = moments
   valid.choices = c("Var","Skewness","Kurtosis")
   if(!as.logical(sum(ExpChoice == valid.choices)))
     stop("\nOptSk-->error: Invalid choices of moments.\n", call. = FALSE)
+  if(is.null(method)) method = "Intepolation"
   if(!is.null(cluster)){
-    foreach(x = 1:length(unique(optdta$date)),.packages = c("OptSK"),.export = "optdta",.combine = "rbind") %dopar% {
+    ans = foreach(x = 1:length(unique(optdta$date)),.packages = c("OptSK"),.export = c("optdta"),.combine = "rbind") %dopar% {
       data <- filter(optdta,date == unique(date)[x])
-      ans <- optdta %>% distinct(date,maturity)
+      ans <- data %>% distinct(date,maturity)
       if(!is.na(match("Var",ExpChoice) == 1)) {
-        Var <- optdta %>% group_by(date)%>%
-          group_by(maturity) %>%
-          summarise(Var = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 1))
-        ans =left_join(ans,Var,by = "maturity")
-      }
+        if(method == "Intepolation"){
+          Var <- data %>% group_by(date)%>%
+            group_by(maturity) %>%
+            summarise(Var = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 1))
+          ans =left_join(ans,Var,by = c("maturity"))
+        } else if(method == "Trapezoidal"){
+          Var <- foreach(i = 1:length(unique(data$maturity)),.combine = "rbind")%do%{
+            tempData <- filter(data,maturity == unique(maturity)[i])
+            tempans <- BKM_trapezoidal(tempData,rate = unique(tempData$RfRate),mat = unique(tempData$maturity)/365,out = 1)
+            Var <- data_frame(date = unique(tempData$date),maturity = unique(tempData$maturity),Var = tempans)
+          }
+          ans =left_join(ans,Var,by = c("maturity","date"))
+        }
+          }
       if(!is.na(match("Skewness",ExpChoice) == 1)) {
-        Skewness <- optdta %>% group_by(date)%>%
-        group_by(maturity) %>%
-        summarise(Skewness = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 2))
-        ans =left_join(ans,Skewness,by = "maturity")
+        if(method == "Intepolation"){
+          Skewness <- data %>% group_by(date)%>%
+            group_by(maturity) %>%
+            summarise(Skewness = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 2))
+          ans =left_join(ans,Skewness,by = "maturity")
+        } else if(method == "Trapezoidal"){
+          Skewness <- foreach(i = 1:length(unique(data$maturity)),.combine = "rbind")%do%{
+            tempData <- filter(data,maturity == unique(maturity)[i])
+            tempans <- BKM_trapezoidal(tempData,rate = unique(tempData$RfRate),mat = unique(tempData$maturity)/365,out = 2)
+            Skewness <- data_frame(date = unique(tempData$date),maturity = unique(tempData$maturity),Skewness = tempans)
+          }
+          ans =left_join(ans,Skewness,by = c("maturity","date"))
+        }
       }
       if(!is.na(match("Kurtosis",ExpChoice) == 1)) {
-      Kurtosis <- optdta %>% group_by(date)%>%
-        group_by(maturity) %>%
-        summarise(Kurtosis = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 3))
-      ans =left_join(ans,Kurtosis,by = "maturity")
+        if(method == "Intepolation"){
+          Kurtosis <- data %>% group_by(date)%>%
+            group_by(maturity) %>%
+            summarise(Kurtosis = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 3))
+          ans =left_join(ans,Kurtosis,by = "maturity")
+        } else if(method == "Trapezoidal"){
+          Kurtosis <- foreach(i = 1:length(unique(data$maturity)),.combine = "rbind")%do%{
+            tempData <- filter(data,maturity == unique(maturity)[i])
+            tempans <- BKM_trapezoidal(tempData,rate = unique(tempData$RfRate),mat = unique(tempData$maturity)/365,out = 3)
+            Kurtosis <- data_frame(date = unique(tempData$date),maturity = unique(tempData$maturity),Kurtosis = tempans)
+          }
+          ans =left_join(ans,Kurtosis,by = c("maturity","date"))
+        }
       }
       }
   } else {
-    foreach(x = 1:length(unique(optdta$date)),.packages = c("OptSK"),.export = "optdta",.combine = "rbind") %do% {
+    ans = foreach(x = 1:length(unique(optdta$date)),.combine = "rbind") %do% {
       data <- filter(optdta,date == unique(date)[x])
-      ans <- optdta %>% distinct(date,maturity)
+      ans <- data %>% distinct(date,maturity)
       if(!is.na(match("Var",ExpChoice) == 1)) {
-        Var <- optdta %>% group_by(date)%>%
-          group_by(maturity) %>%
-          summarise(Var = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 1))
-        ans = left_join(ans,Var,by = "maturity")
+        if(method == "Intepolation"){
+          Var <- data %>% group_by(date)%>%
+            group_by(maturity) %>%
+            summarise(Var = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 1))
+          ans =left_join(ans,Var,by = c("maturity"))
+        } else if(method == "Trapezoidal"){
+          Var <- foreach(i = 1:length(unique(data$maturity)),.combine = "rbind")%do%{
+            tempData <- filter(data,maturity == unique(maturity)[i])
+            tempans <- BKM_trapezoidal(tempData,rate = unique(tempData$RfRate),mat = unique(tempData$maturity)/365,out = 1)
+            Var <- data_frame(date = unique(tempData$date),maturity = unique(tempData$maturity),Var = tempans)
+          }
+          ans =left_join(ans,Var,by = c("maturity","date"))
+        }
       }
       if(!is.na(match("Skewness",ExpChoice) == 1)) {
-        Skewness <- optdta %>% group_by(date)%>%
-          group_by(maturity) %>%
-          summarise(Skewness = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 2))
-        ans = left_join(ans,Skewness,by = "maturity")
+        if(method == "Intepolation"){
+          Skewness <- data %>% group_by(date)%>%
+            group_by(maturity) %>%
+            summarise(Skewness = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 2))
+          ans =left_join(ans,Skewness,by = "maturity")
+        } else if(method == "Trapezoidal"){
+          Skewness <- foreach(i = 1:length(unique(data$maturity)),.combine = "rbind")%do%{
+            tempData <- filter(data,maturity == unique(maturity)[i])
+            tempans <- BKM_trapezoidal(tempData,rate = unique(tempData$RfRate),mat = unique(tempData$maturity)/365,out = 2)
+            Skewness <- data_frame(date = unique(tempData$date),maturity = unique(tempData$maturity),Var = tempans)
+          }
+          ans =left_join(ans,Skewness,by = c("maturity","date"))
+        }
       }
       if(!is.na(match("Kurtosis",ExpChoice) == 1)) {
-        Kurtosis <- optdta %>% group_by(date)%>%
-          group_by(maturity) %>%
-          summarise(Kurtosis = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 3))
-        ans =left_join(ans,Kurtosis,by = "maturity")
+        if(method == "Intepolation"){
+          Kurtosis <- data %>% group_by(date)%>%
+            group_by(maturity) %>%
+            summarise(Kurtosis = BKM_intepolation(mnes = unique(moneyness),vol = unique(ImpVol),rate = unique(RfRate)[1],mat = unique(maturity)/365,out = 3))
+          ans =left_join(ans,Kurtosis,by = "maturity")
+        } else if(method == "Trapezoidal"){
+          Kurtosis <- foreach(i = 1:length(unique(data$maturity)),.combine = "rbind")%do%{
+            tempData <- filter(data,maturity == unique(maturity)[i])
+            tempans <- BKM_trapezoidal(tempData,rate = unique(tempData$RfRate),mat = unique(tempData$maturity)/365,out = 3)
+            Kurtosis <- data_frame(date = unique(tempData$date),maturity = unique(tempData$maturity),Var = tempans)
+          }
+          ans =left_join(ans,Kurtosis,by = c("maturity","date"))
+        }
       }
     }
-    }
+  }
+  return(ans)
 }
